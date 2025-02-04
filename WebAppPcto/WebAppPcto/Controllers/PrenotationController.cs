c72f775796a5e8e673cfac0690924051e086913d
Youness makaoui <119128296+maka-6@users.noreply.github.com>
2025-02-04 14:27:35 +0100

    Classe prenotazioni finita

--- WebAppPcto/WebAppPcto/Controllers/PrenotationController.cs
+++ WebAppPcto/WebAppPcto/Controllers/PrenotationController.cs
@@ -9,7 +9,26 @@ namespace WebAppPcto.Controllers
     [ApiController]
     public class PrenotationController : ControllerBase
     {
-        
+
+        private readonly AgenziaDbContext _context;
+        public PrenotationController(AgenziaDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var result = _context.Prenotation;
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Prenotazione p)
+        {
+            _context.Prenotation.Add(p);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
     }   
 }
 
--- WebAppPcto/WebAppPcto/Controllers/ViaggioController.cs
+++ WebAppPcto/WebAppPcto/Controllers/ViaggioController.cs
@@ -2,19 +2,33 @@
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebAppPcto.Data;
+using Microsoft.AspNetCore.Http;
 
 namespace WebAppPcto.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ViaggioController : Controller
     {
-        public IActionResult Index()
+        private readonly AgenziaDbContext _context;
+        public ViaggioController(AgenziaDbContext context)
         {
-            return View();
+            _context = context;
         }
-        public class Viaggio
+        [HttpGet]
+        public ActionResult Get()
         {
-            [Key]
-            public int id { get; set; }
+            var result = _context.Viagg;
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult Post(viaggio v)
+        {
+            _context.Viagg.Add(v);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
         }
     }
 }
